b3275156d67b570af75e7b442c4346f036b78f5f
Swamy-Pepakayala <98730560+Swamy-Pepakayala@users.noreply.github.com>
2024-03-28 09:14:52 +0530

    Delete provider.tf

--- provider.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  project = "orbital-outpost-416405"
- #  credentials = "${file("C:/cca-terraform/gcp-tf/gcp.keys/keys.json")}"
- # credentials = "${file("/home/tadala1999/gcp-tf/gcp.keys/keys.json")}"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}

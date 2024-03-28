ab3a6b27afcd1c05341a1d45544f3f884c43cf1b
Swamy-Pepakayala <98730560+Swamy-Pepakayala@users.noreply.github.com>
2024-03-28 09:12:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "orbital-outpost-416405"
-   credentials = "${file("C:/cca-terraform/gcp-tf/gcp.keys/keys.json")}"
+ #  credentials = "${file("C:/cca-terraform/gcp-tf/gcp.keys/keys.json")}"
  # credentials = "${file("/home/tadala1999/gcp-tf/gcp.keys/keys.json")}"
   region  = "us-central1"
   zone    = "us-central1-c"
